efa298e58e61b9c2f1dd02413bce20b60016d3d6
acb-code <69542078+acb-code@users.noreply.github.com>
2023-02-10 09:31:49 -0800

    set up test for knn

--- tests/test_tead.py
+++ tests/test_tead.py
@@ -1,11 +1,12 @@
 # tests for tead acquisition
 import pytest
 
-from extremasearch.acquisition.tead import finite_diff
+from extremasearch.acquisition.tead import finite_diff, knn
 import torch
 from botorch.models import SingleTaskGP
 from gpytorch.mlls import ExactMarginalLogLikelihood
 from botorch import fit_gpytorch_mll
+from scipy.stats.qmc import LatinHypercube
 
 dtype = torch.double
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -23,3 +24,17 @@ def example_1d_model():
 def test_finite_diff(example_1d_model):
     assert finite_diff(example_1d_model).shape == torch.Size([20])
     assert finite_diff(example_1d_model).type() == 'torch.FloatTensor'
+
+
+def test_knn(example_1d_model):
+    x = example_1d_model.train_inputs[0].squeeze()
+    lhs_sampler = LatinHypercube(d=1)
+    num_cands = 2000
+    cands = torch.from_numpy(lhs_sampler.random(n=num_cands))
+    dists, inds = knn(x, cands)
+    assert dists.shape == (2000, 1)
+    assert dists.dtype == 'float64'
+    assert inds.shape == (2000, 1)
+    assert inds.dtype == 'int64'
+
+
