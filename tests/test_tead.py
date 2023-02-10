2776eb875637048f9d80e65f0bb3dd232daa6550
acb-code <69542078+acb-code@users.noreply.github.com>
2023-02-10 08:44:51 -0800

    initial test set up successfully

--- tests/test_tead.py
+++ tests/test_tead.py
@@ -10,17 +10,16 @@ from botorch import fit_gpytorch_mll
 dtype = torch.double
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-train_X = torch.rand(20, 1)
-train_Y = torch.sin(train_X).sum(dim=1, keepdim=True)
-model_example = SingleTaskGP(train_X, train_Y)
-mll_example = ExactMarginalLogLikelihood(model_example.likelihood, model_example)
-fit_gpytorch_mll(mll_example)
-
 @pytest.fixture
-def model_fix():
-    return [model_example]
+def example_1d_model():
+    train_X = torch.rand(20, 1)
+    train_Y = torch.sin(train_X).sum(dim=1, keepdim=True)
+    model_example = SingleTaskGP(train_X, train_Y)
+    mll_example = ExactMarginalLogLikelihood(model_example.likelihood, model_example)
+    fit_gpytorch_mll(mll_example)
+    return model_example
 
 
-def test_finite_diff(model_fix):
-    assert finite_diff(model_example).shape == torch.Size([20])
-    assert finite_diff(model_example).type() == 'torch.FloatTensor'
+def test_finite_diff(example_1d_model):
+    assert finite_diff(example_1d_model).shape == torch.Size([20])
+    assert finite_diff(example_1d_model).type() == 'torch.FloatTensor'
