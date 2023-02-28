575a0cab13602ec1b50e36cf23578bdd761b5fba
acb-code <69542078+acb-code@users.noreply.github.com>
2023-02-27 22:19:38 -0800

    iterating on benchmarking for initial canonical problem

--- src/extremasearch/acquisition/tead.py
+++ src/extremasearch/acquisition/tead.py
@@ -105,22 +105,20 @@ def global_tead(model: ExactGP, get_all_candidates: bool = False):
 def get_model_from_piecewise_set(graph, x):
     """Retrieves the local model from the piecewise set at the input point x"""
     graph_leaves = [n for n in graph if graph.out_degree[n] == 0]
-    # bound_list = []
-    # node_list = []
     for n in graph_leaves:
         # get current leaf node
         current_node = graph.nodes()[n]
         current_state = current_node['data']
         current_bounds = current_state.local_bounds
+        # save the lowest node index and highest node index for edge cases
+        if current_bounds[0] <= 0.001 or current_bounds[1] <= 0.001:
+            low_node = n
+        if current_bounds[1] >= 0.999 or current_bounds[0] >= 0.999:
+            high_node = n
+        # check if current node is the right one to use for model
         if current_bounds[0] <= x < current_bounds[1]:
             return current_state.local_model
-        # also save the bounds-node map to use for edge cases
-        # bound_list.append(current_bounds)
-        # node_list.append(n)
-        if current_bounds[0] <= 0.001:
-            low_node = n
-        elif current_bounds[1] >= 0.999:
-            high_node = n
+
     # handle slightly off bound queries
     if x < 0.0:
         # if a prediction lower than 0.0 needed
