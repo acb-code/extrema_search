36076fcb733f09ccf4ef68a0baff6621e2aa1b76
acb-code <69542078+acb-code@users.noreply.github.com>
2023-03-05 15:59:58 -0800

    started working on simplified n-d tead to start out with

--- src/extremasearch/acquisition/qtead.py
+++ src/extremasearch/acquisition/qtead.py
@@ -46,6 +46,121 @@ from botorch.utils import t_batch_mode_transform
 from torch import Tensor
 
 
+class qTaylorExpansionBasedAdaptiveDesign(MCAcquisitionFunction):
+    def __init__(
+            self,
+            model: Model,
+            sampler: Optional[MCSampler] = None,
+    ) -> None:
+        """Using the basic setup from [7] - global model"""
+        super(MCAcquisitionFunction, self).__init__(model=model)
+        if sampler is None:
+            sampler = SobolQMCNormalSampler(sample_shape=torch.Size([512]))
+        self.sampler = sampler
+
+    @t_batch_mode_transform()
+    def forward(self, X: Tensor) -> Tensor:
+        """Evaluate the qTEAD on the candidate set `X`.
+        `b` - t-batch number/dimension
+        `q` - q-batch number/dimension
+        `d` - input dimension (of objective function)
+        `o` - output dimension (of objective function)
+
+        Args:
+            X: A `(b) x q x d`-dim Tensor of `(b)` t-batches with `q` `d`-dim
+                design points each.
+
+        Returns:
+            Tensor: A `(b)`-dim Tensor of Taylor Expansion-Based Adaptive Design
+            values at the given design points `X`={x1, x2, ..., xq}
+        """
+        posterior = self.model.posterior(X)
+        samples = self.get_posterior_samples(posterior)  # n x b x q x o
+        mean = posterior.mean  # b x q x o
+        # TEAD calculations
+        # finite diff gradient estimate at {x, y} data points - could be external, doesn't change as x_c change
+        # generate candidate points x_c via LHS (2000-20000) - could be external? or is it captured in t-batches in opt?
+        # find KNN for candidates x_c compared to {x, y} data points
+        # todo figure out what this should look like
+
+
+###############
+# simplified n-D case
+# setup
+dtype = torch.double
+
+
+# objects
+def nfinite_diff(model):
+    """get approx gradient at model training points"""
+    # assumes 1d input... see implementation in [3] for update to n-D
+    h = 1e-4
+    x = model.train_inputs[0].squeeze()
+    m = len(x)
+    delta_s = torch.zeros(m)
+    for i in range(0, m):
+        x_lo = x[i] - h
+        if x_lo < 0.0:
+            x_lo = torch.DoubleTensor([0.0])
+        x_hi = x[i] + h
+        if x_hi > 1.0:
+            x_hi = torch.DoubleTensor([1.0])
+        y_lo = model(x_lo.unsqueeze(-1)).mean
+        y_hi = model(x_hi.unsqueeze(-1)).mean
+        delta_s[i] = (y_hi - y_lo)/(2*h)
+    return delta_s
+
+
+def nknn(ref, query, k=1):
+    """Function to wrap KNN from sklearn for TEAD setup
+    assumes 1d input dimension, torch Tensors, ref is training inputs
+    query is candidate points, k is number of neighbors
+    """
+    knn_ex = NearestNeighbors(n_neighbors=k).fit(ref.squeeze().numpy().reshape(-1, 1))
+    dists, indices = knn_ex.kneighbors(query.squeeze().numpy().reshape(-1, 1))
+    return dists, indices
+
+
+def nglobal_tead(model: ExactGP, get_all_candidates: bool = False):
+    """Function version of initial implementation TEAD adaptive sampling algorithm
+        Returns candidates and scores, not just final argmax score"""
+    # get training set from model - assuming format expected from ExactGP here
+    x_train = model.train_inputs[0].squeeze()
+    y_train = model.train_targets
+    # calculate gradient at each training point using finite difference
+    grads = nfinite_diff(model)
+    # (this may be available through torch built in behavior, but no time now
+    # generate potential input candidates using LHC samples
+    lhs_sampler = LatinHypercube(d=1)
+    num_cands = 2000
+    cands = torch.from_numpy(lhs_sampler.random(n=num_cands))
+    # calculate nearest neighbors pairings for the candidates
+    dists, inds = nknn(x_train, cands)
+    # calculate the weighting
+    sample_dists = []
+    vals = x_train.numpy()
+    for i in range(len(vals)):
+        for j in range(len(vals)):
+            sample_dists.append(np.linalg.norm(vals[i]-vals[j]))
+    lmax = max(sample_dists)
+    w = torch.ones(num_cands, 1, dtype=dtype) - dists/lmax
+    # do taylor series expansions
+    t = torch.zeros(num_cands, 1, dtype=dtype)
+    s = torch.zeros(num_cands, 1, dtype=dtype)
+    res = torch.zeros(num_cands, 1, dtype=dtype)
+    for i in range(num_cands):
+        g = x_train[inds[i]] - cands[i]
+        t[i, 0] = y_train[inds[i]] + grads[inds[i]]*g
+        s[i, 0] = model(cands[i]).mean
+        res[i, 0] = torch.norm(s[i, 0] - t[i, 0])
+
+    # compute score
+    j = torch.from_numpy(dists/max(dists)) + w * (res / max(res))
+    # pick point with max score
+    if get_all_candidates:
+        return cands, j
+    else:
+        return cands[torch.argmax(j)].unsqueeze(-1)
 
 
 
